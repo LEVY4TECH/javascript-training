d651567feec161f3e734280c29483a1725f78119
LEVY4TECH <levy4tech@gmail.com>
2025-06-03 16:52:16 -0700

    6/3

---
+++ test.js
@@ -0,0 +1,4 @@
+let num1 = Number( document.getElementById("num1").innerHTML)
+let num2 = Number( document.getElementById("num2").innerHTML)
+
+document.getElementById("results").innerHTML = num1 + num2
